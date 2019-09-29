eb432b3f023f1d56ce6c1d2a635413ea6bd8d62b
Praveen Mathew <praveen.mathew92@outlook.com>
2019-09-30 00:35:42 +0530

    Add select all to service

---
+++ src/main/kotlin/com/example/service/QueryService.kt
@@ -0,0 +1,11 @@
+package com.example.service
+
+import com.example.repository.DatabaseClient
+import org.springframework.stereotype.Service
+
+@Service
+class QueryService (val databaseClient: DatabaseClient) {
+    fun getAll() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ src/test/kotlin/com/example/service/QueryServiceTest.kt
@@ -0,0 +1,19 @@
+package com.example.service
+
+import com.example.repository.DatabaseClient
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.Test
+
+class QueryServiceTest{
+    private val databaseClient = mock<DatabaseClient>()
+    private val queryService = QueryService(databaseClient)
+
+    @Test
+    fun `should call select all query form database`() {
+        queryService.getAll()
+
+        verify(databaseClient, times(1)).selectAllQuery()
+    }
+}
